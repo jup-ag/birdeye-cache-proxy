18525fa8ee873fdcdc2767815dec7807f48d0ae1
Sin Wai Kit <31162981+wkitsin@users.noreply.github.com>
2024-06-03 22:03:02 +0800

    feat: history price endpoint (#1)
    * feat: history price endpoint
    
    * feat: floor minute

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,52 @@ const app = new Hono<
   '/'
 >();
 
+app.get('/defi/history_price', async ({ env, req, text, executionCtx }) => {
+  const { BIRDEYE_API_KEY } = env;
+  if (!BIRDEYE_API_KEY) {
+    throw new Error('BIRDEYE_API_KEY is not set');
+  }
+
+  const timeTo = req.query()['time_to'];
+
+  if (!timeTo) {
+    throw new Error('time_to is required');
+  }
+
+  const flooredToMinTimeTo = Math.floor(+timeTo / 100) * 100;
+  const timefrom = flooredToMinTimeTo - 86400; // 1 day ago
+
+  const params = new URLSearchParams({
+    ...req.query(),
+    address_type: 'token',
+    time_from: timefrom.toString(),
+    time_to: flooredToMinTimeTo.toString(),
+  });
+
+  const url = `https://public-api.birdeye.so/defi/history_price?${params.toString()}`;
+
+  const request = new Request(url, {
+    headers: new Headers({
+      'X-API-KEY': BIRDEYE_API_KEY || '',
+    }),
+  });
+
+  let cache = caches.default;
+  const cached = await cache.match(request);
+
+  if (cached) {
+    console.log('cached');
+    return cached;
+  } else {
+    const resp = await fetch(request);
+
+    // save cache
+    executionCtx.waitUntil(cache.put(request, resp.clone()));
+
+    return resp;
+  }
+});
+
 app.get('/defi/*', async ({ env, req, text, executionCtx }) => {
   const { BIRDEYE_API_KEY } = env;
   if (!BIRDEYE_API_KEY) {
@@ -19,6 +65,7 @@ app.get('/defi/*', async ({ env, req, text, executionCtx }) => {
 
   // Add cache API to cache birdeye responses
   let url = req.url.split(req.header('host') || '')[1];
+
   url = `https://public-api.birdeye.so${url}`;
 
   const request = new Request(url, {
